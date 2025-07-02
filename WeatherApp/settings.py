09eae07cd7fe2bceca44ff905c6c374ef6f78008
AvatanshuGupta <avatanshugupta@gmail.com>
2025-07-02 20:10:56 +0530

    Added gunicorn and Procfile for Render deployment

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!h3$^-$%p6bebhe#h8ey)fn46$1+l130j@+3e5lv7xohun@4-^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
